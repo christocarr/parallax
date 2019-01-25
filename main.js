9d42c99c2a19dab87ea213434f192b0a02f67055
christocarr <christocarr@outlook.com>
2019-01-25 15:11:30 +0000

    Add opacity change to firstpage on scroll

--- main.js
+++ main.js
@@ -2,16 +2,22 @@
 const fg = document.querySelector('.foreground');
 const mg = document.querySelector('.midground');
 const bg = document.querySelector('.background');
-const title = document.querySelector('.firstpage h1');
+const title = document.querySelector('.firstpage h1')
+const firstpage = document.querySelector('.firstpage');
 
 document.addEventListener('scroll', (ev) => {
     const Yoff = window.pageYOffset;
     const SCALE_END = 500;
+    const TRANS_END = 800;
     if(Yoff < SCALE_END) {
       const perc = Yoff / SCALE_END;    
       fg.style.transform = `scale(${1 + 0.4 * perc})`;
       mg.style.transform = `scale(${1 + 0.1 * perc})`;
       bg.style.transform = `scale(${1 + 0.04 * perc})`;
-      title.style.transform = `scale(${1 + 0.035 * perc})`;
+      title.style.transform = `scale(${1 + 0.15 * perc})`;
+      firstpage.style.opacity = 1;
+    } else if (Yoff < TRANS_END) {
+        const perc = (Yoff - SCALE_END) / (TRANS_END - SCALE_END);
+        firstpage.style.opacity = `${1 - perc}`;
     }
 });
